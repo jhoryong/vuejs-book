75ae5eae1bacfe6669865605ef87e5b51e6c189b
Horyong Jung <jhoryong@logibros.com>
2022-01-05 17:44:52 +0900

    ch9

---
+++ ch09/styletest/src/components/Child1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    {{msg}}
+    <child11 />
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import Child11 from './Child11.vue'
+
+export default {
+  name: 'child1',
+  components: {Child11},
+  data () {
+    return {
+      msg: 'Child1'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main { border:solid 1px black; background-color:yellow; }
+.test { padding:1px; text-decoration: underline; border: solid 1px black; }
+</style>
---
+++ ch09/styletest/src/components/Child11.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="test">
+    <h3>Child - Child</h3>
+  </div>
+</template>
+
+
+<style scoped>
+.test { font-style:italic }
+</style>
